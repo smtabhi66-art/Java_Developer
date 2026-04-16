b0b8434afcae14c07121a83d5da3a767be76bcf2
smtAbhi66-art <smtabhi66@gmail.com>
2026-04-16 17:24:17 +0000

    Data_type lean

---
+++ Java_Core/Data_type.java
@@ -0,0 +1,30 @@
+// DATA TYPE:-
+
+// Data type -> 1.Primitive          ,                  2.Non Primitive 
+/*               .Integer=> byte,short,int,long,
+                 .float => double(default),float
+                 .char
+                 .boolean
+ */
+
+// File name and class name must be same else it won't run
+
+// Primitive data types below 
+
+class Data_type
+{
+    public static void main(String args[])
+    {
+       int num1 = 9;
+       byte by = 127;
+       short sh = 555;
+       long ll = 123344l;
+
+       float ff = 5.8f;
+       char cc = 'c';
+       boolean bb = true;
+        // System.out.println();
+        // System.out.println("Hello World");
+        // System.out.println(by);
+    }
+}
