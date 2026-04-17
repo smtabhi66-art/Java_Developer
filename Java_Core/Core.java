11c18e8a466f98de1b79bf0c5e25588c11e4bbf1
smtAbhi66-art <smtabhi66@gmail.com>
2026-04-17 14:57:48 +0000

    Type Conversion

---
+++ Java_Core/Core.java
@@ -0,0 +1,19 @@
+public class Core 
+{
+    public static void main(String args[])
+    {
+        // float ff = 5.6f;
+        // int tt = (int) ff;
+        // System.out.println(tt);
+
+        // byte b = 125;
+        // int a = 257;
+        // byte k = (byte)a; // in this the value of is given by like a%128
+        
+        byte a = 10;
+        byte b = 30;
+        int result = a*b;
+
+        System.out.println(result);
+    }
+}
