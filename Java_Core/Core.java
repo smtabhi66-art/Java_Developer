4bab1e440a9ed435464f916fd50e241ab90d59e1
smtAbhi66-art <smtabhi66@gmail.com>
2026-04-17 15:25:39 +0000

    relational operator

--- Java_Core/Core.java
+++ Java_Core/Core.java
@@ -1,24 +1,13 @@
-class Core{
+class Core {
     public static void main(String[] args) {
-        int num = 7;
-        // int num2 = 5;
+        int x = 6;
+        int y = 6;
 
-        // int result = num1+num2;
-        // int result = num1-num2;
-        // int result = num1*num2;
-        // int result = num1/num2;
-        // int result = num1%num2;
+        // relational operator 
+        // <= , >= , == , < ,> all give boolean value
 
-        // num1 = num1 + 2;
-        // increment / decrement
-        // num1++;   =>  post incrememt
-        // it will first fetch the value and then do increment
-        // num1--;    
+        boolean result = x == y;
 
-        // ++num; => pre - incrememnt
-        // in this case it first increment and then fetch the value
-
-
-        System.out.println(num);
+        System.out.println(result);
     }
 }
